db1cce823eae4f7e325f2c04c1dfabe83f566e99
jahanvi03 <64686390+jahanvi03@users.noreply.github.com>
2020-06-07 05:14:04 -0700

    Update script.js

--- week-4/script.js
+++ week-4/script.js
@@ -1,7 +1,7 @@
 (function (window) {
 
 
-var names = ["Jahanvi", "jaanvi", "Teena", "Heril", "Deepak", "anishi", "aman", "Jinisha", "Jack", "jaan"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i in names) {
 
